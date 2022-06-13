ae837c282a9a175f1a383a327de8aaaeb66b1fda
ABulpe <80599594+ABulpe@users.noreply.github.com>
2022-06-13 22:59:52 +0200

    Update env.jsx

--- src/env/env.jsx
+++ src/env/env.jsx
@@ -1,4 +1,4 @@
 export const googleMapKey = 'AIzaSyAOt5PzBT0dGqDoEJ9iLQ9Gqqr1PoP6Xqk'
 
-export const serverApis = ["https://servidor-tfg-production.up.railway.app"]
+export const serverApis = ["https://backend-tfg-production.up.railway.app"]
 // export const serverApis = ['/']
